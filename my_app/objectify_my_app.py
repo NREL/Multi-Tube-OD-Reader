6374e939e293827c3fce7f2c7579c30fd9205adb
shebdon <94721112+shebdon@users.noreply.github.com>
2024-04-09 21:55:21 -0600

    Create class methods for objects

--- my_app/objectify_my_app.py
+++ my_app/objectify_my_app.py
@@ -19,7 +19,7 @@ def save_pickle(path, object):
     with open(path, 'wb') as f:
         pickle.dump(object, f, pickle.DEFAULT_PROTOCOL)
 
-def search_for_new_hardware():
+def device_discovery():
     d = u3.openAllU3()
     device_sns= list(d.keys())
     Close()
@@ -34,102 +34,91 @@ def search_for_new_hardware():
     logger.info("Connected devices: %s", device_sns)
     return hardware
 
-def sort_objects(list_of_objects:list, attribute:str, reversed:bool = False):
-    return list_of_objects.sort(key = lambda x: f"x.{attribute}", reverse = reversed)
-
 def reconcile_hardware(devices:list, hardware:list):
-    #preference given to device records in pickle
-    #additional hardware if different from pickle
     devices = load_pickle().pop(0)
-    hardware = search_for_new_hardware()
+    hardware = device_discovery()
     device_attributes = set((x.id, x.name) for x in devices)
     difference = [ x for x in hardware if (x.id, x.name) not in device_attributes ]
     return devices.append(difference)
-
-def count_available_ports(devices:list):
-    return sum( d.count_available_ports() for d in devices )
-
-
-def ref_ports_in_use(devices:list):
-    """
-    takes list of device objects
-    returns list of tuples of (device object, position)
-    """
-    result = []
-    for d in devices:
-        result = result + [tuple((d, i)) for i in d.report_ref_ports()]
-    return result
-
-def available_test_ports(devices:list):
-    """
-    takes list of device objects
-    returns list of tuples of (device object, position)
-    """
-    result = []
-    for d in devices:
-        result = result + [tuple((d, i)) for i in d.report_available_ports()]
-    return result
-
-def tuples_to_choices(list_of_tuples:list):
-    return {(d, i):f"{d.name}:{i}" for d, i in list_of_tuples}
-
-def regroup_tuples(tuples:list):
-    set1 = set(x for x, y in tuples)
-    nested_list = []
-    for value in set1:
-        nested_list.append([y for x, y in tuples if x is value])
-    return zip(set1, nested_list)
 
 class Device:
     """
     Ports in a device are Port objects. 
     Their position in the list relates to their physical position
     """
+    all_connected = [] #registrar
+
     def __init__(self, name:str, sn, local_id) -> None:
+        #register new device in registrar
+        Device.all_connected.append(self)
         self.name = name
         self.sn = sn
         self.local_id = local_id
-        self.port_usage = tuple(self.Port() for x in range(16))
+        self.ports = []
+        for x in range(1,17):
+            self.Port(f"Port_{x}", x)
 
-    def set_usage(self, changing_ports:list, user:str, assigned_usage:int):
-        for port in changing_ports:
-            self.port_usage[port-1].users.append(user)
-            self.port_usage[port-1].usage = assigned_usage
-
-    def report_available_ports(self):
+    @classmethod
+    def report_available_ports(cls):
         """
         returns list of unused ports
         """
-        return [i+1 for i, x in enumerate(self.port_usage) if x.usage == 0]
+        unused_ports = []
+        for d in cls.all_connected:
+            unused_ports + [p for p in d.ports if p.usage == 0]
+        return unused_ports
     
-    def report_ref_ports(self):
+    @classmethod
+    def count_available_ports(cls):
         """
-        returns list of reference ports
+        returns count of available ports
         """
-        return [i+1 for i, x in enumerate(self.port_usage) if x.usage == 2]
+        return len(cls.report_available_ports())
     
-    def remove_user(self, experiment_name):
-        for port in self.port_usage:
-            if experiment_name in port.users:
-                port.users.remove(experiment_name)
-            if not port.users:
-                port.usage = 0
+    @classmethod
+    def report_ref_ports(cls):
+        """
+        returns list of unused ports
+        """
+        ref_ports = []
+        for d in cls.all_connected:
+            ref_ports + [p for p in d.ports if p.usage == 2]
+        return ref_ports
+    
+    @classmethod
+    def remove_user(cls, experiment_name):
+        for d in cls.all_connected:
+            for p in d.ports:
+                if experiment_name in p.users:
+                    p.users.remove(experiment_name) #list remove
+                if not p.users: # not [] == True
+                    p.usage = 0
+    
+    class Inner(object):
+        pass
 
-    class Port:
-        def __init__(self) -> None:
-            self.users = []
-            self.usage = 0
+    def Port(self, name:str, position:int):
+        device = self
+        class Port(Device.Inner):
+            """
+            position is physical position on device
+            """
+            def __init__(self, name, position) -> None:
+                self.users = []
+                self.usage = 0
+                self.position = position
+                device.ports.append(self)
+        return Port(name, position)
 
-   
+    
 class Timecourse:
     """
-    A port in a timecourse is a tuple of (device, position), unless otherwise indicated
-    "ports" referes to a list of tuples
+
     """
     def __init__(self, name:str, interval:int, ref_port:tuple, test_ports:list) -> None:
         self.name = name
         self.interval = interval
-        self.reference_port = tuple(ref_port)
+        self.reference_port = ref_port
         self.reference_blank = None
         self.test_ports = test_ports
         self.test_blanks = [None for x in test_ports]
@@ -145,20 +134,21 @@ class Timecourse:
         #keep this separate so it can be mocked during testing
         return full_measurement(device.sn, positions, n_reps)
 
-    def measure_blanks(self, ports_tuples:list):
-        for device, positions in regroup_tuples(ports_tuples):
-            readings = self.get_measurements(device, positions, 9)
-            for position, reading in zip(positions, readings):
-                port_tuple = tuple((device, position))
-                if port_tuple in self.test_ports:
-                    self.test_blanks[self.test_ports.index(port_tuple)] = reading
-                    device.set_usage([position], self.name, 1)
-                elif port_tuple == self.reference_port:
-                    self.reference_blank = reading
-                    device.set_usage([position], self.name, 2)
-                else:
-                    logging.warning("The blanked port is neither a reference or test port")
-                    
+    def measure_blanks(self, ports:list):
+        devices = [p.position for p in ports]
+        readings = self.get_measurements(, positions, 9)
+        for p, reading in zip(ports, readings):
+            if p in self.test_ports:
+                self.test_blanks[p.position-1]= reading
+                p.users.append(self.name)
+                p.usage = 1
+            elif p is self.reference_port:
+                self.reference_blank = reading
+                p.users.append(self.name)
+                p.usage = 2
+            else:
+                logging.warning("The blanked port is neither a reference or test port")
+    
     def stop_experiment(self):
-        for device, ports in regroup_tuples(self.test_ports + [self.reference_port]):
-            device.remove_user("self.name")
+        Device.remove_user(self.name)
+                
--- my_app/test_multitube_app.py
+++ my_app/test_multitube_app.py
@@ -25,13 +25,13 @@ def test_add_remove_report_ports_to_device():
     assert device.port_usage[2].users==["third_run"]
 
     device.set_usage([1, 16], "last_run", 1)
-    assert device.report_available_ports()==[2,4,5,6,7,8,9,10,14,15]
+    assert len(device.report_available_ports())==len([2,4,5,6,7,8,9,10,14,15])
 
-    assert device.report_ref_ports()==[3,11,12,13]
+    assert [p.position for p in device.report_ref_ports()]==[3,11,12,13]
 
     device.remove_user("new_run")
-    assert device.report_available_ports()==[2,4,5,6,7,8,9,10,12,13,14,15]
-    assert device.report_ref_ports()==[3,11]
+    assert [p.position for p in device.report_available_ports()]==[2,4,5,6,7,8,9,10,12,13,14,15]
+    assert [p.position for p in device.report_ref_ports()]==[3,11]
 
 def test_device_timecourse_interactions():
     device = Device("Jason", 1323401, 1)
@@ -41,7 +41,7 @@ def test_device_timecourse_interactions():
 
 def test_available_test_ports():
     device = Device("Jason", 1323401, 1)
-    assert available_test_ports([device])==[tuple((device, N)) for N in range(1,17)]
+    assert [p.position for p in available_test_ports([device])]==[N for N in range(1,17)]
 
 def test_timecourse():
     device = Device("Jason", 1323401, 1)
@@ -51,8 +51,8 @@ def test_timecourse():
     assert current_run.test_blanks == [None] * 4 
     assert current_run.blanks_needed()==[tuple((device, x)) for x in range(1,6)]
     #figure out how to mock the "get_measurements function"
-    #current_run.measure_blanks([tuple((device, N)) for N in range(1,4)])
-    #assert current_run.blanks_needed()==[tuple((device, N)) for N in range(4,6)]
+    current_run.measure_blanks([tuple((device, N)) for N in range(1,4)])
+    assert current_run.blanks_needed()==[tuple((device, N)) for N in range(4,6)]
     
 if __name__ == "__main__":
     import pytest 
